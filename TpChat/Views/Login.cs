086ffb2e4bde046daee07f10c3b4472205b59c09
armann-kr <mr_evid@yahoo.com>
2020-04-11 01:59:59 +0430

    Fixed registration recognition also for poor networks which works as well as high speed ones.

--- TpChat/Views/Login.cs
+++ TpChat/Views/Login.cs
@@ -29,7 +29,7 @@ namespace TpChat.Views
         private void Login_Load(object sender, EventArgs e) { }
 
         #region Statics
-        private void JSval(string code, bool close = false)
+        private void JSval(string code, bool closeProgram = false)
         {
             string output;
             using (AutoJSContext context = new AutoJSContext(browser.Window))
@@ -42,7 +42,7 @@ namespace TpChat.Views
                 catch (GeckoException e)
                 {
                     MessageBox.Show(e.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                    if (close)
+                    if (closeProgram)
                     {
                         this.Close();
                         Application.Exit();
@@ -53,7 +53,11 @@ namespace TpChat.Views
             //Application.DoEvents();
         }
         private bool ValidatedUsernameChars() => txtboxUsername.Text.Length > 1;
-        private string FetchMsg() => browser.Document.GetElementsByClassName(Data.CLASS.MSG_Fetch)[0].TextContent;
+        private string FetchMsg()
+        {
+            JSval("$.msgAlert.close()");
+            return browser.Document.GetElementsByClassName(Data.CLASS.MSG_Fetch)[0].TextContent;
+        }
         //
         private static string GetBetween(string strSource, string strStart, string strEnd)
         {
@@ -72,6 +76,86 @@ namespace TpChat.Views
         #endregion
 
         #region Helpers
+        private void InitLoginFun()
+        {
+            // if this app survives ; for further updates =>
+            // JS: in login() dont change $.msgAlert() functions.
+            // C#: use FetchMsg() for fetching the message's text. (will be closed automatically)
+            // Then create Forms for handling the Message.
+            JSval(@"
+function login(e) { // e = this = Window
+    var t = e.username.value;
+    return '' == t || ' ' == t ?
+        (alert({ text: 'لطفا نام خود را وارد کنید' }), !1) :
+        /* Start processing username */
+        ($('#ajax_loader').html(
+            '<center><img height=\'30\' width=\'30\' src=\'/theme/microblog/images/ajax-loader.gif\'></center>'),
+            $.ajax({
+                type: 'POST',
+                url: url('ajax/login/multi'),
+                data: $('#form').serialize(),
+                success: function (e) {
+                    switch (e) {
+                        case 'admins':
+                            alert('درجه دار گرامی توجه کنید : رمز شما ضعیف و نا امن است پسورد خود را پس از ورود به چت روم از تنظمیات تغییر دهید تا این پیام براتون ارسال نشود. پسورد شما باید حداقل 7 کارکتر و شامل حروف و اعداد و حداقل یکی از کاراکتر های ( &,#,$ , % , @ )  باشد . درصورت عدم گذاشتن رمز قوی مسئولیت مشکلات بعدی به عهده شما می باشد . با تشکر');
+                            top.location.href = url($chat_prefix);
+                            break;
+                        case 'adminsmob':
+                            alert('درجه دار گرامی توجه کنید : رمز شما ضعیف و نا امن است پسورد خود را پس از ورود به چت روم از تنظمیات تغییر دهید تا این پیام براتون ارسال نشود. پسورد شما باید حداقل 7 کارکتر و شامل حروف و اعداد و حداقل یکی از کاراکتر های ( &,#,$ , % , @ )  باشد . درصورت عدم گذاشتن رمز قوی مسئولیت مشکلات بعدی به عهده شما می باشد . با تشکر');
+                            top.location.href = url('mobile');
+                            break;
+                        case 'mobile':
+                            top.location.href = url('mobile');
+                            break;
+                        /* case 'captcha':alert({text: 'شما ربات تشخیص داده شدید لطفا کد تصویر امنیتی را وارد کنید'});document.getElementById('lay_captcha').style.display = 'block';break;*/
+                        case 'ok':
+                            top.location.href = url($chat_prefix);
+                            break;
+                        case 'badname':
+                            alert('این نام در لیست سیاه قرار دارد');
+                            break;
+                        case 'benn':
+                            alert('شما اجازه ورود به چتروم را ندارید');
+                            break;
+                        case 'capacity':
+                            alert('ظرفیت چت روم پر است');
+                            break;
+                        case 'running':
+                            document.getElementById('lay_pw').style.display = 'block',
+                                document.getElementById('password').select(),
+                                document.getElementById('lay_gender').style.display = 'none';
+                            break;
+                        case 'questionError':
+                            alert('جواب سئوال امنیتی شما اشتباه است');
+                            break;
+                        case 'passwh':
+                            document.getElementById('lay_pw').style.display = 'block', 
+                            document.getElementById('password').select(), 
+                            document.getElementById('lay_hide').style.display = 'block', 
+                            document.getElementById('lay_gender').style.display = 'none';
+                            break;
+                        case 'pass':
+                            alert('کلمه عبور اشتباه است');
+                            break;
+                        default:
+                            try {
+                                var t = jQuery.parseJSON(e);
+                                'object' == typeof t ? ($('#lay_pw').slideUp(1e3), 
+                                $('#lay_captcha').slideUp(1e3), $('#lay_gender').slideUp(1e3), 
+                                $('#lay_hide').slideUp(1e3), 
+                                $('#lay_security_question').slideDown(1e3)) : 
+                                alert(e);
+                            } catch (n) { alert(e) }
+                    }
+                    $('#ajax_loader').html('')
+                }
+            }),
+            !1
+        )
+    /* End of Username processing */
+} 
+            ");
+        }
         private bool IsPassHidden()
         {
             string output;
@@ -90,38 +174,44 @@ namespace TpChat.Views
                 Data.Joined = true;
         }
 
+        private uint timerInvoked;
         private void GuestCheckLog(object sender, EventArgs e)
         {
-            timer.Enabled = false;
-            CheckJoinStatus();
-            if (Data.Joined)
+            void destroyTimer()
             {
                 timer.Enabled = false;
-                MessageBox.Show("با موفقیت وارد چت روم شدید");
-                // this.Close();
-                // Application.Run(new Home());
+                timer.Dispose();
             }
-            else if (browser.Document.GetElementById(Data.ID.PASSWORD_PARENT) != null)
+
+            if (timerInvoked < 15)
             {
-                if (!IsPassHidden())
+                if (browser.Document.GetElementById(Data.ID.PASSWORD_PARENT) != null)
                 {
-                    timer.Enabled = false;
-                    MessageBox.Show("این نام کاربری ثبت نام شده است", "خطا", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    if (!IsPassHidden())
+                    {
+                        destroyTimer();
+                        MessageBox.Show("این نام کاربری ثبت نام شده است", "خطا",
+                            MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    }
                 }
+                timerInvoked++;
             }
+            else
+                destroyTimer();
         }
         private void GuestLogin()
         {
             var document = browser.Document;
             (document.GetElementById(Data.ID.USERNAME) as Gecko.DOM.GeckoInputElement)
                 .Value = this.txtboxUsername.Text;
-            (document.GetElementById(Data.ID.BUTTON) as Gecko.DOM.GeckoInputElement)
-                .Click();
-
-            this.timer = new Timer();
-            this.timer.Tick += GuestCheckLog;
-            this.timer.Interval = 1000;
-            this.timer.Enabled = true;
+            //
+            JSval("login(this)");
+            //
+            timerInvoked = 0;
+            timer = new Timer();
+            timer.Tick += GuestCheckLog;
+            timer.Interval = 200;
+            timer.Enabled = true;
         }
 
         private void MemberLogin()
@@ -150,16 +240,16 @@ namespace TpChat.Views
         {
             Loader_on();
 
-            JSval("");
-            //if (ValidatedUsernameChars())
-            //{
-            //    if (GuestMode)
-            //        GuestLogin();
-            //    else
-            //        MemberLogin();
-            //}
-            //else
-            //    MessageBox.Show("حداقل طول نام کاربری ۲ حرف می باشد");
+            if (ValidatedUsernameChars())
+            {
+                if (GuestMode)
+                    GuestLogin();
+                else
+                    MemberLogin();
+            }
+            else
+                MessageBox.Show("حداقل طول نام کاربری ۲ حرف می باشد");
+
             Loader_off();
         }
         private void btnHelp_Click(object sender, EventArgs e)
@@ -196,6 +286,7 @@ namespace TpChat.Views
         private void browser_DocumentCompleted(object sender, Gecko.Events.GeckoDocumentCompletedEventArgs e)
         {
             this.Loader_off();
+            this.InitLoginFun();
             CheckJoinStatus();
             if (Data.Joined)
                 MessageBox.Show("شما وارد چت روم شدید");
